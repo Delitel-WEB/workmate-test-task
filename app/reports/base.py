82096f247ebc4cebd73b001e153301205fec5d3f
Delitel <delitel1@mail.ru>
2026-03-17 13:16:06 +0300

    feat: Создан базовый класс отчётов

---
+++ app/reports/base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+
+REPORTS = {}
+
+
+class BaseReport(ABC):
+    """Базовый класс отчётов."""
+
+    name: str = None
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        if cls.name:
+            if cls.name in REPORTS:
+                raise ValueError(f"Дублирующий экземпляр: {cls.name}")
+            REPORTS[cls.name] = cls
+
+    @abstractmethod
+    def build(self) -> None:
+        """Метод создания отчёта."""
+        ...
